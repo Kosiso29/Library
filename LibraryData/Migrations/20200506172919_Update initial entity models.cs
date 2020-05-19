57df4ffadb221e38350fbf0f406345ce23499ba3
Kosi <kafoenyi@gmail.com>
2020-05-19 14:53:49 +0100

    My First Commit

---
+++ LibraryData/Migrations/20200506172919_Update initial entity models.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryData.Migrations
+{
+    public partial class Updateinitialentitymodels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "LibraryBranches",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "LibraryBranches",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "LibraryBranches");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "LibraryBranches",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
